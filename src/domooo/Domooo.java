8166bedd5928d7ca4443330a73293751f8ab09ba
Christophe <millot.christophe@courrier.uqam.ca>
2013-09-25 10:21:00 -0400

    ajout pour la demo

---
+++ src/domooo/Domooo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domooo;
+
+/**
+ *
+ * @author fg191126
+ */
+public class Domooo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+        ///comentaite super pertinent!
+    }
+}
